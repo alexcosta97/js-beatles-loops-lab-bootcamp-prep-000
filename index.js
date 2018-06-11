b521ede3fad382268133e9d51d1a8375d68d2dc6
Alex Paulo da Costa <alex.callum.c@gmail.com>
2018-06-11 14:09:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,3 +17,15 @@ function johnLennonFacts(facts)
   }
   return array;
 }
+
+function iLoveTheBeatles(number)
+{
+  var array = [];
+  do
+  {
+    array.push('I love the Beatles!');
+    number++;
+  }while(number < 15)
+  
+  return array;
+}
